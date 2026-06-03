427b2b58821cb481e4f4051352cb3456fc56f7e1
Zühre <zuhrenur78a@gmail.com>
2026-06-03 15:06:46 +0300

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,20 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
 
 export default config;
